461d89721ff1ed8234108c0b0449d3a79d37f38b
Vinicius Arruda <viniarruda.souza@gmail.com>
2019-04-17 20:15:30 +0000

    Create route for html

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const http = require("http");
 const express = require("express");
 const fields = require('./fields.json');
 const app = express();
-const index = require('./src/index.html');
 
 app.get("/fields", function(req, res) {
   res.send(fields);
 });
 
-app.get('/', function(req, res){
-  res.sendfile(index);
+app.get('/',function(req,res) {
+  res.sendFile('index.html', { root: __dirname + '/src/' });
+});
+
+app.get('/scripts',function(req,res) {
+  res.sendFile('script.js', { root: __dirname + '/src/assets/js/' });
+});
+
+app.get('/styles',function(req,res) {
+  res.sendFile('style.css', { root: __dirname + '/src/assets/css/' });
 });
 
 http.createServer(app).listen(3000, () => console.log("Servidor rodando local na porta 3000"));
